4d7464a8b0bce59fff34c8cf8bed3671983de57d
rodrigo-mendoza <rodrigo.mendozaarbizu@mail.mcgill.ca>
2016-09-29 15:48:58 -0400

    first commit

---
+++ Socket-Server/src/clientserver/TCPClient.java
@@ -0,0 +1,12 @@
+package clientserver;
+//require packages to call inputBuffer and socket connection.
+import java.io.*;
+import java.net.*;
+public class TCPClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
