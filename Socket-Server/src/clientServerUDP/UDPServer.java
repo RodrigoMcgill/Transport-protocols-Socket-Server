05b617492688393ab9abd226b33c7d3aa9a35d67
rodrigo-mendoza <rodrigo.mendozaarbizu@mail.mcgill.ca>
2016-09-30 18:58:07 -0400

    modified stuff

---
+++ Socket-Server/src/clientServerUDP/UDPServer.java
@@ -0,0 +1,54 @@
+package clientServerUDP;
+
+/**
+ * UDPServer
+ * 
+*  @author: Rodrigo Mendoza and Katwar 
+ *
+ */
+import java.net.*;
+
+public class UDPServer {
+	public static void main(String args[]) throws Exception
+	{
+		// Create a UDP socket on port 9876
+		DatagramSocket serverSocket = new DatagramSocket(1155);
+		
+		// Allocate space for the received and response messages 
+		byte[] receiveData = new byte[500];
+		byte[] sendData = new byte[500];
+		
+		// Enter an infinite loop
+		while (true)
+		{
+			// Allocate space to receive an incoming packet
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			
+			// Receive a packet from the client
+			// This call blocks until a packet is received
+			serverSocket.receive(receivePacket);
+			
+			// If we get here, then we received a packet
+			System.out.println("Packet received...");
+
+			String sentence = new String(receivePacket.getData());
+			System.out.println("From client: " + sentence);
+
+			// Grab the sender's IP address and port
+			InetAddress ipAddress = receivePacket.getAddress();
+			int port = receivePacket.getPort();
+			
+			// Convert the sentence to all caps
+			String capitalizedSentence = sentence.toUpperCase();
+			
+			// Convert the String to a byte array
+			sendData = capitalizedSentence.getBytes();
+			
+			// Make the UDP packet with the response message
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+			
+			// Send the UDP packet back to the client
+			serverSocket.send(sendPacket);
+		}
+	}
+}
